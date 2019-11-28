39242a811d3f7f44ae2ad8a2261953beb31f17a0
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-11-28 19:17:03 +0700

    OK

--- latihan06-db-oop/db_hrd.sql
+++ latihan06-db-oop/db_hrd.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2019 at 06:56 PM
--- Server version: 10.1.10-MariaDB
+-- Generation Time: 28 Nov 2019 pada 13.08
+-- Versi Server: 10.1.10-MariaDB
 -- PHP Version: 5.5.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -23,7 +23,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pegawai`
+-- Struktur dari tabel `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `idjabatan` int(11) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `honor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jabatan`
+--
+
+INSERT INTO `jabatan` (`idjabatan`, `jabatan`, `honor`) VALUES
+(1, 'Staff HR', 2100000),
+(3, 'Staff Produksi', 2300000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pegawai`
 --
 
 CREATE TABLE `pegawai` (
@@ -37,8 +57,21 @@ CREATE TABLE `pegawai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `pegawai`
+--
+
+INSERT INTO `pegawai` (`id_pegawai`, `nama`, `alamat`, `jkelamin`, `lulusan`, `foto`, `input`) VALUES
+(11, 'kjlkj', 'lkjlkj', 'Wanita', 'SMA', '30f676ec3b0213ca7ca7b259e2e603bf47d45df1.png', '2019-11-28 12:07:43');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`idjabatan`);
 
 --
 -- Indexes for table `pegawai`
@@ -51,10 +84,15 @@ ALTER TABLE `pegawai`
 --
 
 --
+-- AUTO_INCREMENT for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `idjabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `pegawai`
 --
 ALTER TABLE `pegawai`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
